2cb7aa99cb35b7570ac122d56fe14778b4f635fa
Quentin Vannarath <qvannarath@gmail.com>
2023-06-22 22:17:28 +0200

    ✨[ROOM] Added booked times table

--- api/models/room.model.js
+++ api/models/room.model.js
@@ -6,12 +6,61 @@ module.exports = (sequelize, Sequelize) => {
             primaryKey: true
         },
         name: {
-            type: Sequelize.STRING
+            type: Sequelize.STRING,
+            allowNull: false
+        },
+        image: {
+            type: Sequelize.STRING,
+            allowNull: false
         },
         capacity: {
-            type: Sequelize.INTEGER
+            type: Sequelize.INTEGER,
+            allowNull: false
+        },
+        floor: {
+            type: Sequelize.STRING,
+            allowNull: false
+        },
+        pointOfContactName: {
+            type: Sequelize.STRING,
+            allowNull: false
+        },
+        pointOfContactEmail: {
+            type: Sequelize.STRING,
+            allowNull: false
         }
     });
+
+    const BookedTime = sequelize.define("booked_times", {
+        id: {
+            type: Sequelize.INTEGER,
+            autoIncrement: true,
+            primaryKey: true
+        },
+        startDate: {
+            type: Sequelize.STRING,
+            allowNull: false
+        },
+        endDate: {
+            type: Sequelize.STRING,
+            allowNull: false
+        },
+        purposeOfBooking: {
+            type: Sequelize.STRING,
+            allowNull: false
+        },
+        isModerator: {
+            type: sequelize.BOOLEAN,
+            defaultValue: false
+        }
+    });
+
+    Room.hasMany(BookedTime, {
+        foreignKey: {
+            allowNull: false
+        }
+    });
+    BookedTime.belongsTo(Room);
 
     return Room;
 };
