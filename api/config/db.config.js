9db814f8f33c4a4ef4237606a9f3ec4ffd2f77e3
Quentin Vannarath <qvannarath@gmail.com>
2023-06-25 00:31:57 +0200

    🔒️ [ENV] Added environment variables

--- api/config/db.config.js
+++ api/config/db.config.js
@@ -1,8 +1,10 @@
+require('dotenv').config()
+
 module.exports = {
-    HOST: "localhost",
-    USER: "root",
-    PASSWORD: "root",
-    DB: "api_smart",
+    HOST: process.env.DB_HOST,
+    USER: process.env.DB_USER,
+    PASSWORD: process.env.DB_PASSWORD,
+    DB: process.env.DB_NAME,
     dialect: "mysql",
     dialectOptions: {
         socketPath: '/Applications/MAMP/tmp/mysql/mysql.sock'
--- api/server.js
+++ api/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const errorHandler = require("./middleware/errorHandler");
+require('dotenv').config()
 
 const app = express();
 
@@ -30,7 +31,7 @@ require('./routes/booking.routes')(app);
 app.use(errorHandler);
 
 // set port, listen for requests
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
