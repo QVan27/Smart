477d89a35162fe8a4038a37f76bd57d1b0e10b2a
Quentin Vannarath <qvannarath@gmail.com>
2023-06-24 14:27:03 +0200

    🤡 [ADD-USERS] Replacing random email by firstname and lastname

--- api/server/seeders/20230620222848-add-users.js
+++ api/server/seeders/20230620222848-add-users.js
@@ -4,16 +4,22 @@ const bcrypt = require('bcryptjs');
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    const users = Array.from({ length: 30 }).map(() => ({
-      firstName: faker.person.firstName(),
-      lastName: faker.person.lastName(),
-      email: faker.internet.email(),
-      position: faker.helpers.arrayElement(["developpeur", "designer", "marketing", "UX/UI", "manager"], { min: 1, max: 1 }),
-      picture: faker.image.avatar(),
-      password: bcrypt.hashSync('password123', 8), // Remplacer 'password123' par le mot de passe souhaité
-      createdAt: new Date(),
-      updatedAt: new Date(),
-    }));
+    const users = Array.from({ length: 30 }).map(() => {
+      const firstName = faker.person.firstName();
+      const lastName = faker.person.lastName();
+      const email = `${firstName.toLowerCase()}.${lastName.toLowerCase()}@smart.com`;
+
+      return {
+        firstName,
+        lastName,
+        email,
+        position: faker.helpers.arrayElement(["developpeur", "designer", "marketing", "UX/UI", "manager"], { min: 1, max: 1 }),
+        picture: faker.image.avatar(),
+        password: bcrypt.hashSync('password123', 8), // Remplacer 'password123' par le mot de passe souhaité
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      };
+    });
 
     await queryInterface.bulkInsert('Users', users);
   },
