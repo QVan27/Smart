2e32248161cf20e60fb18ca3d12936f0cc0338c2
Quentin Vannarath <qvannarath@gmail.com>
2023-06-28 17:56:28 +0200

    💄 [BOOKINGS] updated box shadow

--- smart-app/pages/index.js
+++ smart-app/pages/index.js
@@ -85,7 +85,7 @@ const BookingCard = styled.div`
       height: 1.25rem;
       border-radius: 50%;
       overflow: hidden;
-      box-shadow: 5px 5px 20px 0px rgba(0, 0, 0, 0.15);
+      box-shadow: rgba(0, 0, 0, 0.25) 0px 0.0625em 0.0625em, rgba(0, 0, 0, 0.25) 0px 0.125em 0.5em, rgba(255, 255, 255, 0.1) 0px 0px 0px 1px inset;
 
       img {
         width: 100%;
