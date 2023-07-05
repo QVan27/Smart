d48694b5f86edebe673167b3c10fb64516b51c69
Quentin Vannarath <qvannarath@gmail.com>
2023-07-05 13:12:14 +0200

    🚀 [APP] Try new address for production

--- api/server.js
+++ api/server.js
@@ -6,7 +6,7 @@ require('dotenv').config()
 const app = express();
 
 const corsOptions = {
-  origin: process.env.CORS_ORIGIN
+  origin: 'https://smart-app-prod.netlify.app/'
 };
 
 app.use(cors(corsOptions));
